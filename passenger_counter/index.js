9370278200adeff9398a3183cd6d9ad3ee9fa112
yousefdheede <58173109+yousefdheede@users.noreply.github.com>
2023-07-03 21:14:05 +0300

    Add files via upload
    passenger counter using html , java script and css

---
+++ passenger_counter/index.js
@@ -0,0 +1,37 @@
+
+// let age =23
+// let dog= 7
+
+// let mydogage=age*dog 
+ 
+// console.log(mydogage) //42
+
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+let count=0
+let countEl=document.getElementById("count-el")
+console.log(countEl)
+function inc(){
+    count=count+1
+    countEl.innerText=count
+    console.log(count)
+}
+
+
+
